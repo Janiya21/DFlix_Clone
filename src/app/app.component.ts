5cf4a803fd95ef5197c3b5ed84ff2f226f09a414
Janiya21 <janithsandaru999@gmail.com>
2022-11-08 10:33:19 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,59 @@ export class AppComponent {
     dots: false,
     navSpeed: 700,
     items:6,
-    navText: ['', ''],
+    navText: ['<=', '=>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+
+  customOptions2: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    items:6,
+    navText: ['<=', '=>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
+
+  customOptions3: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    items:6,
+    navText: ['<=', '=>'],
     responsive: {
       0: {
         items: 1
